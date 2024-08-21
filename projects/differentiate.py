cffb08544a6061bf1d94c2ec01aec85f00ac354c
GauteJ1 <gautej.post@gmail.com>
2024-08-21 17:00:47 +0200

    Completed exercise 1.2

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,25 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    d = np.zeros_like(u)
+    Nt = len(u) - 1
+    for n in range(len(u)):
+        if n == 0:
+            d[0] = (u[1] - u[0]) / dt
+        elif n == Nt:
+            d[Nt] = (u[Nt] - u[Nt - 1]) / dt
+        else:
+            d[n] = (u[n + 1] - u[n - 1]) / (2 * dt)
+    return d
+
 
 def differentiate_vector(u, dt):
-    pass
+    d = np.zeros_like(u)
+    d[1:-1] = (u[2:] - u[0:-2]) / (2 * dt)
+    d[0] = (u[1] - u[0]) / dt
+    d[-1] = (u[-1] - u[-2]) / dt
+    return d
+
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -15,6 +30,11 @@ def test_differentiate():
     du2 = differentiate_vector(u, dt)
     assert np.allclose(du1, du2)
 
-if __name__ == '__main__':
+    # Add extra test against exact solution according to sub-problem a)
+    du = 2 * t
+    # Note that our numerical method is not precise at the endpooints
+    assert np.allclose(du[1:-1], du1[1:-1])
+
+
+if __name__ == "__main__":
     test_differentiate()
-    
