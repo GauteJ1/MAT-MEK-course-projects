d69f723ed1d7536e177b95573fda17b11e21c30d
GauteJ1 <gautej.post@gmail.com>
2024-09-04 10:34:38 +0200

    Fix exponential_growth and add 1-3 on VibFD

--- projects/VibFD.py
+++ projects/VibFD.py
@@ -7,11 +7,15 @@ where w is a constant and f(t) is a source term assumed to be 0.
 We use various boundary conditions.
 
 """
+
 import numpy as np
+from math import factorial
 import matplotlib.pyplot as plt
 import sympy as sp
+from scipy import sparse
 
-t = sp.Symbol('t')
+t = sp.Symbol("t")
+
 
 class VibSolver:
     """
@@ -20,6 +24,7 @@ class VibSolver:
         u'' + w**2 u = f,
 
     """
+
     def __init__(self, Nt, T, w=0.35, I=1):
         """
         Parameters
@@ -45,13 +50,12 @@ class VibSolver:
             Number of time steps
         """
         self.Nt = Nt
-        self.dt = self.T/Nt
-        self.t = np.linspace(0, self.T, Nt+1)
+        self.dt = self.T / Nt
+        self.t = np.linspace(0, self.T, Nt + 1)
 
     def ue(self):
-        """Return exact solution as sympy function
-        """
-        return self.I*sp.cos(self.w*t)
+        """Return exact solution as sympy function"""
+        return self.I * sp.cos(self.w * t)
 
     def u_exact(self):
         """Exact solution of the vibration equation
@@ -73,7 +77,7 @@ class VibSolver:
         """
         u = self()
         ue = self.u_exact()
-        return np.sqrt(self.dt*np.sum((ue-u)**2))
+        return np.sqrt(self.dt * np.sum((ue - u) ** 2))
 
     def convergence_rates(self, m=4, N0=32):
         """
@@ -97,17 +101,23 @@ class VibSolver:
         """
         E = []
         dt = []
-        self.set_mesh(N0) # Set initial size of mesh
+        self.set_mesh(N0)  # Set initial size of mesh
         for m in range(m):
-            self.set_mesh(self.Nt+10)
+            self.set_mesh(self.Nt + 10)
             E.append(self.l2_error())
             dt.append(self.dt)
-        r = [np.log(E[i-1]/E[i])/np.log(dt[i-1]/dt[i]) for i in range(1, m+1, 1)]
+        r = [
+            np.log(E[i - 1] / E[i]) / np.log(dt[i - 1] / dt[i])
+            for i in range(1, m + 1, 1)
+        ]
         return r, np.array(E), np.array(dt)
 
     def test_order(self, m=5, N0=100, tol=0.1):
         r, E, dt = self.convergence_rates(m, N0)
+        print(np.array(r))
+        print(self.order)
         assert np.allclose(np.array(r), self.order, atol=tol)
+
 
 class VibHPL(VibSolver):
     """
@@ -115,15 +125,17 @@ class VibHPL(VibSolver):
 
     Boundary conditions u(0)=I and u'(0)=0
     """
+
     order = 2
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
+        u = np.zeros(self.Nt + 1)
         u[0] = self.I
-        u[1] = u[0] - 0.5*self.dt**2*self.w**2*u[0]
+        u[1] = u[0] - 0.5 * self.dt**2 * self.w**2 * u[0]
         for n in range(1, self.Nt):
-            u[n+1] = 2*u[n] - u[n-1] - self.dt**2*self.w**2*u[n]
+            u[n + 1] = 2 * u[n] - u[n - 1] - self.dt**2 * self.w**2 * u[n]
         return u
+
 
 class VibFD2(VibSolver):
     """
@@ -133,6 +145,7 @@ class VibFD2(VibSolver):
 
     The boundary conditions require that T = n*pi/w, where n is an even integer.
     """
+
     order = 2
 
     def __init__(self, Nt, T, w=0.35, I=1):
@@ -141,8 +154,22 @@ class VibFD2(VibSolver):
         assert T.is_integer() and T % 2 == 0
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
+        A = sparse.diags(
+            [1 / (self.dt**2), (self.w**2 - 2 / (self.dt**2)), 1 / (self.dt**2)],
+            np.array([-1, 0, 1]),
+            (self.Nt + 1, self.Nt + 1),
+            "csr",
+        )
+        A[0, :2] = 1, 0
+        A[-1, -2:] = 0, 1
+
+        b = np.zeros(self.Nt + 1)
+        b[0] = self.I
+        b[-1] = self.I
+
+        u = sparse.linalg.spsolve(A, b)
         return u
+
 
 class VibFD3(VibSolver):
     """
@@ -153,6 +180,7 @@ class VibFD3(VibSolver):
 
     The boundary conditions require that T = n*pi/w, where n is an even integer.
     """
+
     order = 2
 
     def __init__(self, Nt, T, w=0.35, I=1):
@@ -161,8 +189,23 @@ class VibFD3(VibSolver):
         assert T.is_integer() and T % 2 == 0
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
+        A = sparse.diags(
+            [1 / (self.dt**2), (self.w**2 - 2 / (self.dt**2)), 1 / (self.dt**2)],
+            np.array([-1, 0, 1]),
+            (self.Nt + 1, self.Nt + 1),
+            "csr",
+        )
+        A[0, :2] = 1, 0
+        # Made second order stencil for u'(T) by hand using Taylor expansions of u^{T-1} and u^{T-2}
+        A[-1, -3:] = 1 / (2 * self.dt), -2 / self.dt, 3 / (2 * self.dt)
+
+        b = np.zeros(self.Nt + 1)
+        b[0] = self.I
+        b[-1] = 0
+
+        u = sparse.linalg.spsolve(A, b)
         return u
+
 
 class VibFD4(VibFD2):
     """
@@ -172,18 +215,58 @@ class VibFD4(VibFD2):
 
     The boundary conditions require that T = n*pi/w, where n is an even integer.
     """
+
     order = 4
 
     def __call__(self):
-        u = np.zeros(self.Nt+1)
+        A = sparse.diags(
+            [
+                -1 / (12 * self.dt**2),
+                16 / (12 * self.dt**2),
+                self.w**2 - 30 / (12 * self.dt**2),
+                16 / (12 * self.dt**2),
+                -1 / (12 * self.dt**2),
+            ],
+            np.array([-2, -1, 0, 1, 2]),
+            (self.Nt + 1, self.Nt + 1),
+            "csr",
+        )
+        A[1, :6] = (
+            10 / (12 * self.dt**2),
+            self.w**2 - 15 / (12 * self.dt**2),
+            -4 / (12 * self.dt**2),
+            14 / (12 * self.dt**2),
+            -6 / (12 * self.dt**2),
+            1 / (12 * self.dt**2),
+        )
+
+        A[-2, -6:] = (
+            1 / (12 * self.dt**2),
+            -6 / (12 * self.dt**2),
+            14 / (12 * self.dt**2),
+            -4 / (12 * self.dt**2),
+            self.w**2 - 15 / (12 * self.dt**2),
+            10 / (12 * self.dt**2),
+        )
+
+        A[0, :4] = 1, 0, 0, 0
+        A[-1, -4:] = 0, 0, 0, 1
+
+        b = np.zeros(self.Nt + 1)
+        b[0] = self.I
+        b[-1] = self.I
+
+        u = sparse.linalg.spsolve(A, b)
         return u
+
 
 def test_order():
     w = 0.35
-    VibHPL(8, 2*np.pi/w, w).test_order()
-    VibFD2(8, 2*np.pi/w, w).test_order()
-    VibFD3(8, 2*np.pi/w, w).test_order()
-    VibFD4(8, 2*np.pi/w, w).test_order(N0=20)
+    VibHPL(8, 2 * np.pi / w, w).test_order()
+    VibFD2(8, 2 * np.pi / w, w).test_order()
+    VibFD3(8, 2 * np.pi / w, w).test_order()
+    VibFD4(8, 2 * np.pi / w, w).test_order(N0=20)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_order()
